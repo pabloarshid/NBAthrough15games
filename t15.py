5160d4e6f29adca6ed778b435e8b263377ea5cbb
PabloArshid <umarsa.birvancity@gmail.com>
2018-11-16 11:33:17 -0800

    cleaned Lebrons Data

--- t15.py
+++ t15.py
@@ -1,7 +1,22 @@
 import numpy as np
 import pandas as pd
-from nba_py import player
+import glob
+import os
+import matplotlib
+matplotlib.use('Agg')
+import matplotlib.pyplot as plt
+import seaborn as sns
 
-get_player = player.get_player("Lebron", last_name="James")
+l = [pd.read_csv(filename, header = 0,parse_dates=['Date']) for filename in glob.glob("/mnt/c/Users/umar_/Documents/Projects/Data/NBA/Through 15 games/*.csv")]
+means = [ ]
 
-print(get_player)
+for df in l:
+    df['Year'] = df.Date.dt.year
+    temp = df[df["G"] < 16]
+    year = temp.Date.dt.year.unique()
+    temp2 = temp[["PTS", "AST", "TRB", "STL", "BLK", "TOV", "Year"]].mean(axis=0)
+    means.append(temp2)
+    
+meansdf = pd.DataFrame(means)
+meansdf["Year"] = meansdf.Year.astype(int)
+meansdf = meansdf.set_index('Year')
